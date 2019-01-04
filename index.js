bce33298df5a462d69e4189db32e625d47fb4da7
William Ulman <will.ulman@gmail.com>
2019-01-04 00:46:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObject = Object.assign({}, object, { [key]: value });
   return newObject;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
