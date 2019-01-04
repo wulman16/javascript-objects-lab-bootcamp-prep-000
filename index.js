9927d01b84f7edd8047f7a67ee56d44a995934ed
William Ulman <will.ulman@gmail.com>
2019-01-04 00:47:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, obj);
+  delete newObject[key];
+  return newObject;
+}
