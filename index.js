76a5f1d44d0b9ddbc66c290b681361cf092a1712
William Ulman <will.ulman@gmail.com>
2019-01-04 00:41:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign({}, object, {key: value});
+  var newObject = Object.assign(object, {key: value});
   return newObject;
 }
-
